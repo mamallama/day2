1997ada1b243a815dd727e491b806810d894ba19
Llamamama <reneeq25@gmail.com>
2015-09-01 17:23:46 -0400

    final, normal hw

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,32 +1,35 @@
+def repeat_check (array, guess)
+  if  array.include?(guess.to_i)
+      puts "You already tried that number..."
+  end
+end
+
 guess  = 0
 tries = 0
 comp_guess = rand(0..101)
 array = []
-def (repeat_check)
-  if  array.include?(guess.to_i)
-      puts "You already tried that number..."
-  else array << guess.to_i
-end
+
 
 puts "I'm thinking of a number between 1-100. Guess my number."
 
 until tries == 5
 
   guess = gets.chomp
-
   tries += 1
-  puts "HINT: Computer thinks: #{comp_guess}"
-  puts "SO FAR: #{array.to_s}"
-  repeat_check
+  #puts "HINT: Computer thinks: #{comp_guess}"
+  #puts "SO FAR: #{array.to_s}"
+
   if guess.to_i > comp_guess && tries < 5
-    puts "Too high, try again."
+    puts "Too high!!"
 
   elsif guess.to_i < comp_guess && tries < 5
-    puts "Too low, try again."
+    puts "Nope."
   elsif guess.to_i == comp_guess
     puts "JACKPOT!"
     break
   elsif
     puts "Sorry, you lost."
   end
+  repeat_check(array, guess)
+  array << guess.to_i
 end
